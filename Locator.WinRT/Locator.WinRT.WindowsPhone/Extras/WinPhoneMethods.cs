3e7a1eac68b6ae01dbe389ed8a15f8b9e05affdb
Flush Arcade <info@flusharcade.com>
2016-07-16 21:27:31 +1000

    Replaced WinPhone version with WinRT version.

---
+++ Locator.WinRT/Locator.WinRT.WindowsPhone/Extras/WinPhoneMethods.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WinPhoneMethods.cs" company="Flush Arcade">
+//   Copyright (c) 2015 Flush Arcade All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Locator.WinPhone.Extras
+{
+    using Locator.Portable.Extras;
+
+    using Windows.UI.Xaml;
+
+    /// <summary>
+    /// The methods interface
+    /// </summary>
+    public class WinPhoneMethods : IMethods
+    {
+        public void Exit()
+        {
+            Application.Current.Exit();
+        }
+    }
+}
+
---
+++ Locator.WinRT/Locator.WinRT.WindowsPhone/Modules/WinPhoneModule.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WinPhoneModule.cs" company="Flush Arcade">
+//   Copyright (c) 2015 Flush Arcade All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Locator.WinPhone.Modules
+{   
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Autofac;
+
+    using Locator.WinPhone.Location;
+    using Locator.WinPhone.Extras;
+    using Locator.Portable.Location;
+    using Locator.Portable.Extras;
+    using Locator.Portable.Ioc;
+
+    public class WinPhoneModule : IModule
+    {
+        public void Register(ContainerBuilder builer)
+        {
+            builer.RegisterType<GeolocatorWinPhone>().As<IGeolocator>().SingleInstance();
+            builer.RegisterType<WinPhoneMethods>().As<IMethods>().SingleInstance();
+        }
+    }
+}
